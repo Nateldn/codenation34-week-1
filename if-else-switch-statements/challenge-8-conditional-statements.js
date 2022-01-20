eb2da86274ba58c9ddfa71a003eaf151dfa77c08
Nateldn <32700665+Nateldn@users.noreply.github.com>
2022-01-20 01:11:11 +0000

    Complete Challenge 8

--- if-else-switch-statements/challenge-8-conditional-statements.js
+++ if-else-switch-statements/challenge-8-conditional-statements.js
@@ -1,11 +1,20 @@
-let num1 = 4;
-let num2 = 5;
+
+num1 = Math.ceil(Math.random()*100); // Generate a random number between 1 and 100
+num2 = Math.ceil(Math.random()*100); // Generate a random number between 1 and 100
+numberSum = num1 + num2; // Sum of two numbers
+numberMultiple = num1 * num2; // Multiply the two numbers
 
 
-const numSum = (num1, num2) => {
-    return num1 + num2;
+const calculateEvenOrNot = () => {  // function to see if sum is even
+    if (numberSum % 2 === 0) { // Does the remainder of the sum when divided by 2 equal zero?
+        console.log(`${numberSum}` ); // Print the sum of the number to the screen
+    }
+    else { // If not print the multiple of the number
+        console.log(`The numbers ${num1} and ${num2} were multipled together to make ${numberMultiple} as the sum of the two numbers (${numberSum}) is an odd number.`);
+    }
     
 }
 
+calculateEvenOrNot();
 
 
