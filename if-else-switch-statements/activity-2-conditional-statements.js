05d325f2d7d1bb8d563a8519ac04848d405ad1ea
Nateldn <32700665+Nateldn@users.noreply.github.com>
2022-01-20 02:58:48 +0000

    Refactor Activity 2 Code

--- if-else-switch-statements/activity-2-conditional-statements.js
+++ if-else-switch-statements/activity-2-conditional-statements.js
@@ -1,18 +1,40 @@
 // Activity 2: Pizza Determine if this is a favourite ingredient 
-    const favTopping = "Mushrooms";
+    let toppings = ["Pepperoni", "Ham", "Sausage", "Capers", "Garlic", "Bacon", "Mushrooms", "Green Peppers", "Anchovies" ];
 
-    switch (favTopping) {
-        case "Pepperoni":
-        break
-        case "Ham":
-        break
-        case "Mushrooms":
-        break
-        case "Green Peppers":
-        break
-        case "Anchovies":
-        break
+    const lowerCaseToppings = toppings.map(toppings => toppings.toLocaleLowerCase());
+    console.log(lowerCaseToppings[0]);
+   
+    let toppingSelection = Math.floor(Math.random() * toppings.length); // Get a random topping from the array
+//console.log(toppingSelection);
+    switch (toppingSelection) {
+        case "Pepperoni".toLowerCase():
+            console.log(`${toppingSelection} is an important ingredint for my pizza.`);
+        break;
+        case "Ham".toLowerCase():
+            console.log(`${toppingSelection} is an important ingredint for my pizza.`);
+        break;
+        case "Sausage".toLowerCase():
+            console.log(`${toppingSelection} is an important ingredint for my pizza.`);
+        break;
+        case "Capers".toLowerCase():
+            console.log(`I don’t mind having ${toppingSelection} on my pizza.`);
+        break;
+        case "Garlic".toLowerCase():
+            console.log(`I don’t mind having ${toppingSelection} on my pizza.`);
+        break;
+        case "Bacon".toLowerCase():
+            console.log(`I don’t mind having ${toppingSelection} on my pizza.`);
+        break;
+        case "Mushrooms".toLowerCase():
+            console.log(`${toppingSelection} should not be on my pizza.`);
+        break;
+        case "Green Peppers".toLowerCase():
+            console.log(`${toppingSelection} should not be on my pizza.`);
+        break;
+        case "Anchovies".toLowerCase():
+            console.log(`${toppingSelection} should not be on my pizza.`);
+        break;
 
         default: 
-        console.log("I don’t mind having ${topping} on my pizza.");
+            console.log(`I don’t mind having ${toppingSelection} on my pizza.`);
     } 
