dd92aeaa2c84fe992fd30086ef844845061011c8
Nateldn <32700665+Nateldn@users.noreply.github.com>
2022-01-20 10:01:14 +0000

    Refactor to fix index of an item from the toppings array being logged to the console [unsuccessful].

--- if-else-switch-statements/activity-2-conditional-statements.js
+++ if-else-switch-statements/activity-2-conditional-statements.js
@@ -1,12 +1,17 @@
 // Activity 2: Pizza Determine if this is a favourite ingredient 
-    let toppings = ["Pepperoni", "Ham", "Sausage", "Capers", "Garlic", "Bacon", "Mushrooms", "Green Peppers", "Anchovies" ];
+    let toppings = ["Pepperoni", "Ham", "Sausage", "Capers", "Garlic", "Bacon", "Mushrooms", "Green Peppers", "Anchovies" ]; // Array of toppings
+    //let topping = "Anchovies"; 
 
-    const lowerCaseToppings = toppings.map(toppings => toppings.toLocaleLowerCase());
-    console.log(lowerCaseToppings[0]);
-   
-    let toppingSelection = Math.floor(Math.random() * toppings.length); // Get a random topping from the array
+    let lowerCaseToppings = toppings.map(toppings => toppings.toLowerCase());
+    //console.log(lowerCaseToppings[0]);
+
+    for (let i = 0; i < lowerCaseToppings.length; i++ ) {
+       console.log(lowerCaseToppings[i])
+    }
+
+    let toppingSelection = Math.floor(Math.random() * lowerCaseToppings.length); // Get a random topping from the array
 //console.log(toppingSelection);
-    switch (toppingSelection) {
+    switch (toppingSelection) { // switch statements
         case "Pepperoni".toLowerCase():
             console.log(`${toppingSelection} is an important ingredint for my pizza.`);
         break;
@@ -17,13 +22,13 @@
             console.log(`${toppingSelection} is an important ingredint for my pizza.`);
         break;
         case "Capers".toLowerCase():
-            console.log(`I don’t mind having ${toppingSelection} on my pizza.`);
+            console.log(`${toppingSelection} is an important ingredint for my pizza.`);
         break;
         case "Garlic".toLowerCase():
-            console.log(`I don’t mind having ${toppingSelection} on my pizza.`);
+            console.log(`${toppingSelection} is an important ingredint for my pizza.`);
         break;
         case "Bacon".toLowerCase():
-            console.log(`I don’t mind having ${toppingSelection} on my pizza.`);
+            console.log(`${toppingSelection} is an important ingredint for my pizza.`);
         break;
         case "Mushrooms".toLowerCase():
             console.log(`${toppingSelection} should not be on my pizza.`);
@@ -36,5 +41,5 @@
         break;
 
         default: 
-            console.log(`I don’t mind having ${toppingSelection} on my pizza.`);
+            console.log(`I don’t mind having ${toppingSelection[i]} on my pizza.`); // Currently outputs the index of the top
     } 
